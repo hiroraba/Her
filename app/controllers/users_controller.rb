a81b69e8b8146061693d472e2158f0674815f51c
hiroraba <hiroraba@gmail.com>
2013-10-26 16:54:33 +0900

    add sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -26,49 +27,46 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to todo list webapp!"
+    else
+      render 'new'
     end
   end
+end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
+# PATCH/PUT /users/1
+# PATCH/PUT /users/1.json
+def update
+  respond_to do |format|
+    if @user.update(user_params)
+      format.html { redirect_to @user, notice: 'User was successfully updated.' }
       format.json { head :no_content }
+    else
+      format.html { render action: 'edit' }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
     end
   end
+end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+# DELETE /users/1
+# DELETE /users/1.json
+def destroy
+  @user.destroy
+  respond_to do |format|
+    format.html { redirect_to users_url }
+    format.json { head :no_content }
+  end
+end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+private
+# Use callbacks to share common setup or constraints between actions.
+def set_user
+  @user = User.find(params[:id])
+end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
