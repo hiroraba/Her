f288b791c0dad86162297d05741280beb882a9b7
hiroraba <hiroraba@gmail.com>
2013-11-14 00:00:03 +0900

    add no content test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe "Users" do
+
+  let(:user) { FactoryGirl.create(:user) }
+  
+  subject { page }
+
+  describe "Can't access user page" do
+    before { visit user_path(user) }
+    it { should have_selector("h1", :text => "No Contents") }
+  end
+  
+  describe "Can't access users page" do
+    before { visit users_path }
+    it { should have_selector("h1", :text => "No Contents") }
+  end
 end
