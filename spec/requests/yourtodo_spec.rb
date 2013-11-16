ae480f090d80085945257b1a7edc8b48bf02d33b
hiroraba <hiroraba@gmail.com>
2013-11-16 14:21:28 +0900

    Add test

--- spec/requests/yourtodo_spec.rb
+++ spec/requests/yourtodo_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe "your todo page" do
 
   let(:user) { FactoryGirl.create(:user) }
+  let(:other_user) { FactoryGirl.create(:user, name: "others", email: "others@others.co.jp") }
   let!(:t1) { FactoryGirl.create(:todolist, user: user, content: "Fooooooo", todotime: 1.hour.ago) }
   let!(:t2) { FactoryGirl.create(:todolist, user: user, content: "Barrrrrr", todotime: 1.hour.ago) }
+  let!(:t3) { FactoryGirl.create(:todolist, user: other_user, content: "OTHER_Barrrrrr", todotime: 1.hour.ago) }
 
   before do
     visit signin_path
@@ -13,11 +15,16 @@ describe "your todo page" do
     click_button "Sign in"
   end
 
+
   subject { page }
 
-  describe "todolist" do
+  describe "you can read your todolist" do
     it { should have_content(t1.content) }
     it { should have_content(t2.content) }
     it { should have_content(user.todolists.count) }
+  end
+
+  describe "you can't read others todolist" do
+    it { should_not have_content(t3.content) }
   end
 end
